ba8a2e50ea89a284dba64505afad9d0673cb52a6
Christopher O'Driscoll <chrisodriscoll@hotmail.com>
2024-12-06 13:59:33 +0100

    added conditions to regex and simple if statements for control

--- 3/three.js
+++ 3/three.js
@@ -10,18 +10,26 @@ fs.readFile(filePath, 'utf8', (err, data) => {
         return;
     }
 
-    const commands = data.trim().match(/mul\(\d{1,3},\d{1,3}\)/g);
+    const commands = data.trim().match(/do\(\)|don\'t\(\)|mul\(\d{1,3},\d{1,3}\)/g);
 
     let sum = 0;
+    let enabled = true;
 
     if (commands) {
         commands.forEach(command => {
+            console.log('Command:', command);
+            if (command === 'do()') {
+                enabled = true;
+            } else if (command === 'don\'t()') {
+                enabled = false;
+            } else if (command.startsWith('mul(') && enabled) {
             const match = command.match(/\d{1,3}/g);
-            if (match) {
-                const num1 = parseInt(match[0], 10);
-                const num2 = parseInt(match[1], 10);
-                
-                sum += num1 * num2;
+                if (match) {
+                    const num1 = parseInt(match[0], 10);
+                    const num2 = parseInt(match[1], 10);
+                    
+                    sum += num1 * num2;
+                }
             }
         });
     } else {
